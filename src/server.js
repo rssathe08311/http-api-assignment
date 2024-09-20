46d83711be3337039c9776e79d9febebfe2ce7b1
Reva Sathe <rss9622@g.rit.edu>
2024-09-19 20:25:58 -0400

    Midway commit for API assignment

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+
+const responseHandler = require('./responses.js');
+
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    '/': responseHandler.getIndex,
+    '/success': responseHandler.success,
+    '/badRequest': responseHandler.badRequest,
+    '/unauthorized': responseHandler.unauthorized,
+    '/forbidden': responseHandler.forbidden,
+    '/internal': responseHandler.internal,
+    '/notImplemented': responseHandler.notImplemented,
+    '/style.css': responseHandler.getStyle,
+    'notFound': responseHandler.notFound,
+}
+
+
+
+const onRequest = (request, response) => {
+    const protocol = request.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+    request.acceptedTypes = request.headers.accept.split(',');
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
+    if (urlStruct[parsedUrl.pathname]) {
+        urlStruct[parsedUrl.pathname](request, response);
+    } else {
+        // otherwise send them to the index (normally this would be the 404 page)
+        urlStruct.index(request, response);
+    }
+}
+
+
+
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1: ${port}`);
+});
